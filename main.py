2baebe4c1ea463c074d9b6ccbf22c2f2713b8ce5
Vinay Byagari <50073633+vinaykumarb2424@users.noreply.github.com>
2024-12-01 20:12:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ class ReadGmails(object):
                                     # print(part)
                                     if part.get_content_type() == "text/plain":
                                         body = part.get_payload(decode=True)
+                                        body = body.decode('utf-8', 'ignore')
                                         filtered_text =self.clean_body(body)
                                         if filtered_text:
                                             data = self.create_json_object(subject.strip(), filtered_text)
